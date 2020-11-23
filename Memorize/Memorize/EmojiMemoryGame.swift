bd9da01dd39c2342e1008b21e1fca82ebdeddc45
Dias <47509783+daudias@users.noreply.github.com>
2020-11-23 22:58:27 +0600

    deleted theme struct

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -23,23 +23,4 @@ class EmojiMemoryGame: ObservableObject {
     func choose(card: MemoryGame<String>.Card) {
         model.choose(card: card)    
     }
-    
-    struct Theme {
-        let name: String
-        let color: Color
-        let emojis: [String]
-        var numberOfCardsToShow: Int
-        
-        init(name: String, color: Color, emojis: [String]) {
-            self.name = name
-            self.emojis = emojis
-            self.color = color
-            numberOfCardsToShow = Int.random(in: 1...emojis.count)
-        }
-        
-        init(name: String, color: Color, emojis: [String], numberOfCardsToShow: Int) {
-            self.init(name: name, color: color, emojis: emojis)
-            self.numberOfCardsToShow = min(numberOfCardsToShow, emojis.count)
-        }
-    }
 }
